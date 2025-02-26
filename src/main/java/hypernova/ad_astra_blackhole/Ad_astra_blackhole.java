b7207f0c1fb88cbe5724f2a659b3863f0df4888c
HyperNova <reczek.thomas@gmail.com>
2025-02-26 16:50:18 +0000

    Base Build

---
+++ src/main/java/hypernova/ad_astra_blackhole/Ad_astra_blackhole.java
@@ -0,0 +1,10 @@
+package hypernova.ad_astra_blackhole;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ad_astra_blackhole implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/hypernova/ad_astra_blackhole/client/Ad_astra_blackholeClient.java
@@ -0,0 +1,10 @@
+package hypernova.ad_astra_blackhole.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ad_astra_blackholeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
